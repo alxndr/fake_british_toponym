a02c795534407df6f2499006074a6d3be3df99d0
Alexander <alxndr+github@gmail.com>
2014-10-11 08:25:44 -0700

    it's a gem!
    [closes #2]

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem "probability"
+
 group :development do
   gem "guard"
   gem "guard-rspec", require: false
--- spec/fake_british_toponym_spec.rb
+++ spec/fake_british_toponym_spec.rb
@@ -1,33 +1,28 @@
 require "spec_helper"
 
-MANY = 10e4.to_i
-
 describe FakeBritishToponym do
+
+  MANY = 10e3.to_i
 
   it "should exist" do
     expect(FakeBritishToponym).to be_truthy
   end
 
-  it "should be a string" do
-    expect(FakeBritishToponym).to be_a String
-  end
+  describe "options" do
 
-
-  pending "is this thing on?", 'options' do
-
-    describe 'modifier' do
-      describe 'when set to false' do
-        it 'should never be more than one word or include punctuation' do
+    describe "modifier" do
+      describe "when set to false" do
+        it "should never be more than one word or include punctuation" do
           MANY.times do
             toponym = FakeBritishToponym.new(modifier: false)
-            expect(toponym).to_not include ' '
-            expect(toponym).to_not include '-'
+            expect(toponym).to_not include " "
+            expect(toponym).to_not include "-"
             expect(toponym).to_not include "'"
           end
         end
       end
 
-      it 'should not contain obvious errors' do
+      it "should not contain obvious errors" do
         DOUBLE_CAPITAL_REGEX = /{[A-Z]{2}/
         DOUBLE_NONALPHA_REGEX = /[^[:alpha:]]{2}/
 
@@ -36,15 +31,15 @@ describe FakeBritishToponym do
           [DOUBLE_CAPITAL_REGEX, DOUBLE_NONALPHA_REGEX].each do |regex|
             expect(toponym).not_to match regex
           end
-          [' -', '- ', " '", "' "].each do |bad_char_sequence|
+          [" -", "- ", " '", "' "].each do |bad_char_sequence|
             expect(toponym).not_to include bad_char_sequence
           end
         end
       end
     end
 
-    describe 'min_syllables' do
-      it 'should return words of roughly appropriate length' do
+    describe "min_syllables" do
+      it "should return words of roughly appropriate length" do
         MANY.times do
           length = rand(10)
           toponym = FakeBritishToponym.new(min_syllables: length)
